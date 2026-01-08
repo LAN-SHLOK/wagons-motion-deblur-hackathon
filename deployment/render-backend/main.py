c24ce152a7d4f74a83909cd627c3a533c8c7ee44
LAN-SHLOK <shlokpatel699@gmail.com>
2026-01-08 23:02:33 +0530

    Update main.py

---
+++ deployment/render-backend/main.py
@@ -0,0 +1,49 @@
+     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 153.6/153.6 kB 34.0 MB/s eta 0:00:00
+Collecting anyio<5,>=3.6.2
+  Downloading anyio-4.12.1-py3-none-any.whl (113 kB)
+     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 113.6/113.6 kB 26.5 MB/s eta 0:00:00
+Collecting mpmath<1.4,>=1.1.0
+  Obtaining dependency information for mpmath<1.4,>=1.1.0 from https://files.pythonhosted.org/packages/43/e3/7d92a15f894aa0c9c4b49b8ee9ac9850d6e63b03c9c32c0367a13ae62209/mpmath-1.3.0-py3-none-any.whl.metadata
+  Downloading mpmath-1.3.0-py3-none-any.whl.metadata (8.6 kB)
+Collecting MarkupSafe>=2.0
+  Downloading markupsafe-3.0.3-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl (20 kB)
+Collecting exceptiongroup>=1.0.2
+  Downloading exceptiongroup-1.3.1-py3-none-any.whl (16 kB)
+Downloading numpy-1.26.4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (18.2 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 18.2/18.2 MB 91.7 MB/s eta 0:00:00
+Downloading https://download.pytorch.org/whl/cpu/torch-2.9.1%2Bcpu-cp310-cp310-manylinux_2_28_x86_64.whl (184.4 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 184.4/184.4 MB 9.7 MB/s eta 0:00:00
+Downloading https://download.pytorch.org/whl/cpu/torchvision-0.24.1%2Bcpu-cp310-cp310-manylinux_2_28_x86_64.whl (1.9 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 1.9/1.9 MB 81.1 MB/s eta 0:00:00
+Downloading fsspec-2025.12.0-py3-none-any.whl (201 kB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 201.4/201.4 kB 40.8 MB/s eta 0:00:00
+Downloading networkx-3.4.2-py3-none-any.whl (1.7 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 1.7/1.7 MB 94.4 MB/s eta 0:00:00
+Downloading sympy-1.14.0-py3-none-any.whl (6.3 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 6.3/6.3 MB 119.5 MB/s eta 0:00:00
+Downloading mpmath-1.3.0-py3-none-any.whl (536 kB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 536.2/536.2 kB 73.7 MB/s eta 0:00:00
+Using cached numpy-1.26.4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (18.2 MB)
+Using cached https://download.pytorch.org/whl/cpu/torch-2.9.1%2Bcpu-cp310-cp310-manylinux_2_28_x86_64.whl (184.4 MB)
+Using cached https://download.pytorch.org/whl/cpu/torchvision-0.24.1%2Bcpu-cp310-cp310-manylinux_2_28_x86_64.whl (1.9 MB)
+Using cached fsspec-2025.12.0-py3-none-any.whl (201 kB)
+Using cached networkx-3.4.2-py3-none-any.whl (1.7 MB)
+Using cached sympy-1.14.0-py3-none-any.whl (6.3 MB)
+Using cached mpmath-1.3.0-py3-none-any.whl (536 kB)
+Installing collected packages: simsimd, pytz, mpmath, websockets, uvloop, urllib3, tzdata, typing-extensions, sympy, stringzilla, six, pyyaml, python-multipart, python-dotenv, pyparsing, psutil, polars-runtime-32, pillow, packaging, numpy, networkx, MarkupSafe, kiwisolver, idna, httptools, h11, fsspec, fonttools, filelock, cycler, click, charset_normalizer, certifi, annotated-types, annotated-doc, uvicorn, typing-inspection, tifffile, scipy, requests, python-dateutil, pydantic-core, polars, opencv-python-headless, opencv-python, lazy-loader, jinja2, imageio, exceptiongroup, contourpy, torch, scikit-image, pydantic, pandas, matplotlib, anyio, albucore, watchfiles, ultralytics-thop, torchvision, starlette, seaborn, albumentations, ultralytics, fastapi
+Successfully installed MarkupSafe-3.0.3 albucore-0.0.24 albumentations-2.0.8 annotated-doc-0.0.4 annotated-types-0.7.0 anyio-4.12.1 certifi-2026.1.4 charset_normalizer-3.4.4 click-8.3.1 contourpy-1.3.2 cycler-0.12.1 exceptiongroup-1.3.1 fastapi-0.128.0 filelock-3.20.2 fonttools-4.61.1 fsspec-2025.12.0 h11-0.16.0 httptools-0.7.1 idna-3.11 imageio-2.37.2 jinja2-3.1.6 kiwisolver-1.4.9 lazy-loader-0.4 matplotlib-3.10.8 mpmath-1.3.0 networkx-3.4.2 numpy-1.26.4 opencv-python-4.11.0.86 opencv-python-headless-4.11.0.86 packaging-25.0 pandas-2.3.3 pillow-12.1.0 polars-1.36.1 polars-runtime-32-1.36.1 psutil-7.2.1 pydantic-2.12.5 pydantic-core-2.41.5 pyparsing-3.3.1 python-dateutil-2.9.0.post0 python-dotenv-1.2.1 python-multipart-0.0.21 pytz-2025.2 pyyaml-6.0.3 requests-2.32.5 scikit-image-0.25.2 scipy-1.15.3 seaborn-0.13.2 simsimd-6.5.12 six-1.17.0 starlette-0.50.0 stringzilla-4.6.0 sympy-1.14.0 tifffile-2025.5.10 torch-2.9.1+cpu torchvision-0.24.1+cpu typing-extensions-4.15.0 typing-inspection-0.4.2 tzdata-2025.3 ultralytics-8.3.249 ultralytics-thop-2.0.18 urllib3-2.6.3 uvicorn-0.40.0 uvloop-0.22.1 watchfiles-1.1.1 websockets-15.0.1
+[notice] A new release of pip is available: 23.0.1 -> 25.3
+[notice] To update, run: pip install --upgrade pip
+==> Uploading build...
+==> Uploaded in 22.8s. Compression took 6.7s
+==> Build successful 🎉
+==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
+==> Deploying...
+==> Running 'uvicorn main:app --host 0.0.0.0 --port 10000'
+ERROR:    Error loading ASGI app. Attribute "app" not found in module "main".
+==> Exited with status 1
+==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
+==> Running 'uvicorn main:app --host 0.0.0.0 --port 10000'
+ERROR:    Error loading ASGI app. Attribute "app" not found in module "main".
+==> Running 'uvicorn main:app --host 0.0.0.0 --port 10000'
+ERROR:    Error loading ASGI app. Attribute "app" not found in module "main".
